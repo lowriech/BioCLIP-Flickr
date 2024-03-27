cdfcdcd1d9dc110daeb4e3293ece2dd9579e13e4
Christopher J Lowrie <chlowrie@Christophers-MacBook-Pro.local>
2024-03-27 15:44:56 -0700

    First commit

---
+++ src/Python/app/bio_clip.py
@@ -0,0 +1,77 @@
+import open_clip
+import uuid, urllib
+import PIL
+import torch
+import numpy as np
+import json
+from torchvision import transforms
+import torch.nn.functional as F
+import collections
+import heapq
+min_prob = 1e-9
+
+model, preprocess_train, preprocess_val = open_clip.create_model_and_transforms('hf-hub:imageomics/bioclip')
+tokenizer = open_clip.get_tokenizer('hf-hub:imageomics/bioclip')
+txt_emb_npy = "txt_emb_species.npy"
+txt_names_json = "txt_emb_species.json"
+txt_emb = torch.from_numpy(np.load(txt_emb_npy, mmap_mode="r"))
+with open(txt_names_json) as fd:
+    txt_names = json.load(fd)
+print(txt_emb)
+
+preprocess_img = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.Resize((224, 224), antialias=True),
+        transforms.Normalize(
+            mean=(0.48145466, 0.4578275, 0.40821073),
+            std=(0.26862954, 0.26130258, 0.27577711),
+        ),
+    ]
+)
+
+ranks = ("Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species")
+
+k = 5
+rank = 6
+
+def format_name(taxon, common):
+    taxon = " ".join(taxon)
+    if not common:
+        return taxon
+    return f"{taxon} ({common})"
+
+def classify_image(remote_image, what_animal):
+    presences = ["present", "not present"]
+    tmp_id = str(uuid.uuid4())
+    f = f"/tmp/{tmp_id}.jpg"
+    urllib.request.urlretrieve( 
+        remote_image, 
+        f
+    )
+    img = PIL.Image.open(f)
+    img = preprocess_val(img)
+    img_features = model.encode_image(img.unsqueeze(0))
+    img_features = F.normalize(img_features, dim=-1)
+    logits = (model.logit_scale.exp() * img_features @ txt_emb).squeeze()
+    probs = F.softmax(logits, dim=0)
+
+    # If predicting species, no need to sum probabilities.
+    if rank + 1 == len(ranks):
+        topk = probs.topk(k)
+        print(topk)
+        return {
+            format_name(*txt_names[i]): prob for i, prob in zip(topk.indices, topk.values)
+        }
+    # print(probs)
+
+    # # Sum up by the rank
+    # output = collections.defaultdict(float)
+    # for i in torch.nonzero(probs > min_prob).squeeze():
+    #     output[" ".join(txt_names[i][0][: rank + 1])] += probs[i]
+
+    # topk_names = heapq.nlargest(k, output, key=output.get)
+    # with open('./verifications.txt', 'w') as f:
+    #     f.write(topk_names)
+            
+
---
+++ src/Python/app/flickr.py
@@ -0,0 +1,28 @@
+import requests, json, urllib
+
+
+BASE_URL = "https://www.flickr.com/services/rest/"
+API_KEY = "1c6bc9eda634d3b3d2cc63174d3e8ddb"
+BBOX = "-82.09,24.765,-79.181,27.092"
+D1 = "2022-01-01"
+D2 = "2022-01-30"
+BASE_ARGS = {
+    "bbox": BBOX, 
+    "min_taken_date": D1,
+    "max_taken_date": D2,
+    "extras": "url_o",
+    "text": "wildlife"
+}
+
+
+def get_flickr(args):
+    args["method"] = "flickr.photos.search"
+    args["api_key"] = API_KEY
+    args["format"] = "json"
+    args["nojsoncallback"] = 1
+    # args["license"] = 9
+    encoded_params = urllib.parse.urlencode(args)
+
+    full_url = f"{BASE_URL}?{encoded_params}"
+    r = requests.get(full_url)
+    return json.loads(r.content)
