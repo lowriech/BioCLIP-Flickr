27bcf1a6e29a69b75e42e8c7f649a0f8e39773f6
Felipe Montealegre-Mora <felimomo@berkeley.edu>
2024-03-28 15:57:47 +0000

    fixed download script

--- src/Python/app/download_species.py
+++ src/Python/app/download_species.py
@@ -4,14 +4,14 @@ import os
 def download_hf_input_files():
     hf_hub_download(
         repo_id = "imageomics/bioclip-demo",
-        repo_type="model",
+        repo_type="space",
         filename = "txt_emb_species.npy",
         local_dir= os.path.join(script_abspath, "..", "..", "..", "data"),
         local_dir_use_symlinks = False,
     )
     hf_hub_download(
         repo_id = "imageomics/bioclip-demo",
-        repo_type="model",
+        repo_type="space",
         filename = "txt_emb_species.json",
         local_dir= os.path.join(script_abspath, "..", "..", "..", "data"),
         local_dir_use_symlinks = False,
