bdc75ea853b70e04c26fba285a870d01d3a49846
Felipe Montealegre-Mora <felimomo@berkeley.edu>
2024-03-28 18:38:07 +0000

    adding iucn redlist download, minor changes to species name download

--- src/Python/download_species.py
+++ src/Python/download_species.py
@@ -1,22 +1,31 @@
 from huggingface_hub import hf_hub_download
 import os
 
-def download_hf_input_files():
+def download_species_names(target_path):
     hf_hub_download(
         repo_id = "imageomics/bioclip-demo",
         repo_type="space",
         filename = "txt_emb_species.npy",
-        local_dir= os.path.join(script_abspath, "..", "..", "..", "data"),
+        local_dir= target_path,
         local_dir_use_symlinks = False,
     )
     hf_hub_download(
         repo_id = "imageomics/bioclip-demo",
         repo_type="space",
         filename = "txt_emb_species.json",
-        local_dir= os.path.join(script_abspath, "..", "..", "..", "data"),
+        local_dir= target_path,
         local_dir_use_symlinks = False,
     )
 
+def species_threat(species_id):
+    #
+    # token taken from example https://apiv3.iucnredlist.org/api/v3/docs#threat-name
+    token = "9bb4facb6d23f48efbf424bb05c0c1ef1cf6f468393bc745d42179ac4aca5fee"
+    end_point = f"/api/v3/threats/species/id/{species_id}?token='{token}'"
+    base_url = "https://apiv3.iucnredlist.org"
+    
+
 if __name__ == '__main__':
     script_abspath = os.path.abspath(__file__)
-    download_hf_input_files()
+    target_path = os.path.join(script_abspath, "..", "..", "..", "data")
+    download_species_names(target_path)
